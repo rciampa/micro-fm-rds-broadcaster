916526ac235ec22bd58af8da9da096359e902a17
Richard Cimapa <rciampa@csumb.edu>
2016-04-01 21:25:45 -0700

    Changed folder name

---
+++ RpiFmBroadcaster/main.c
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Richard Ciampa
+ *
+ * Created on March 16, 2016, 4:49 PM
+ */
+
+/*
+ * Includes in alpha order
+ */
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Constants and macros
+ */
+
+
+/*
+ * Function prototypes
+ */
+
+bool broadcaster_start();
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+    
+    return (EXIT_SUCCESS);
+}
+
+bool broadcaster_start(){
+    
+ 
+    return true;
+}
